3d302283d5ce0a6422242ea06d64bac65129be6d
arkadacasinos <bezdep.rf@mail.ru>
2026-04-11 01:30:58 +0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,16 +1,12 @@
 import { NextResponse } from 'next/server';
 export function middleware(req) {
-  const url = req.nextUrl;
   const country = req.headers.get('x-vercel-ip-country') || 'US';
-  const ua = req.headers.get('user-agent') || '';
-  const allowedCountries = ['RU', 'BY', 'KZ', 'AM', 'AZ', 'KG', 'TJ', 'UZ', 'MD'];
-  const isSuspiciousBot = /HeadlessChrome|Lighthouse|PageSpeed|SemrushBot|AhrefsBot|DotBot|PetalBot/i.test(ua);
-  if (!allowedCountries.includes(country) || isSuspiciousBot) {
-    return new NextResponse(null, { status: 403 });
+  const allowed = ['RU', 'BY', 'KZ', 'AM', 'AZ', 'KG', 'TJ', 'UZ', 'MD'];
+  if (!allowed.includes(country)) {
+    return new NextResponse('Access Denied', { status: 403 });
   }
-
   return NextResponse.next();
 }
 export const config = {
-  matcher: '/:path*',
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
 };
